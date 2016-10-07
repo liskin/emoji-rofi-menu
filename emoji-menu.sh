aacf24d385c8fc313a9ffdc8c0d317c10629cb5f
Rob Bolton <rab26@aber.ac.uk>
2016-10-07 12:54:33 +0100

    Added clipboard support

--- emoji-menu.sh
+++ emoji-menu.sh
@@ -4,5 +4,13 @@ cd $(dirname $(readlink -f $0))
 
 line=$(cat emoji-data | rofi -dmenu)
 if [ ! -z "$line" ]; then
-    xdotool key $(cut -f 2 <<< "$line" )
+    char=$(cut -f 1 -d' ' <<< "$line")
+    xdotool key $(cut -f 2 <<< "$line")
+    if command -v xsel >/dev/null; then
+        echo -n "$char" | xsel -i -p
+        echo -n "$char" | xsel -i -b
+    elif command -v xclip >/dev/null; then
+        echo -n "$char" | xsel -i -sel prim
+        echo -n "$char" | xsel -i -sel clip
+    fi
 fi
