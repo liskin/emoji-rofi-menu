629a986904393dcf955030a23aa7856e3d7f0eba
Tomas Janousek <tomi@nomi.cz>
2019-06-04 13:31:00 +0200

    Drop clipboard support

--- emoji-menu.sh
+++ emoji-menu.sh
@@ -9,10 +9,3 @@ read -r <<<"$emoji_desc" emoji desc
 [[ $emoji && $desc && $key ]]
 
 xdotool key "$key"
-if command -v xsel >/dev/null; then
-	echo -n "$emoji" | xsel -i -p
-	echo -n "$emoji" | xsel -i -b
-elif command -v xclip >/dev/null; then
-	echo -n "$emoji" | xclip -i -sel prim
-	echo -n "$emoji" | xclip -i -sel clip
-fi
