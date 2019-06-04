cfc2fa81bcac369a4fd127ef9e938f5314112a10
Tomas Janousek <tomi@nomi.cz>
2019-06-04 18:01:56 +0200

    Enlarge font size and width

--- emoji-menu.sh
+++ emoji-menu.sh
@@ -4,7 +4,7 @@ set -eu
 
 cd "$(dirname "$(readlink -f "$0")")"
 
-IFS=$'\t' read -r emoji_desc key < <(rofi -dmenu -i -p "emoji" <emoji-data)
+IFS=$'\t' read -r emoji_desc key < <(rofi -dmenu -i -p "emoji" -font "mono 20" -width 80 <emoji-data)
 read -r <<<"$emoji_desc" emoji desc
 [[ $emoji && $desc && $key ]]
 
