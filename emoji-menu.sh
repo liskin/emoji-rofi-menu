391c9cd98add6dca869731b12c160cbc82dd31fa
Tomas Janousek <tomi@nomi.cz>
2019-12-12 20:40:57 +0000

    Try harder :-(

--- emoji-menu.sh
+++ emoji-menu.sh
@@ -7,4 +7,8 @@ cd "$(dirname "$(readlink -f "$0")")"
 read -r emoji desc < <(rofi -dmenu -i -p "emoji" -font "mono 20" -width 80 <emoji-data)
 [[ $emoji && $desc ]]
 
-xdotool type "$emoji"
+# wait for focus to return back from rofi
+sleep 0.2
+
+xdotool type --clearmodifiers --delay 0 "$emoji"
+xclip -rmlastnl <<<"$emoji"
