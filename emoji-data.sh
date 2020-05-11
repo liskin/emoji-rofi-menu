8508932ba9b8bd660e1a5c83b8ba7e3ec61c192d
Tomas Janousek <tomi@nomi.cz>
2020-05-11 12:49:38 +0200

    Add derived emojis as well

--- emoji-data.sh
+++ emoji-data.sh
@@ -3,8 +3,10 @@
 set -eu
 
 curl -Ssf -L \
-	-O https://github.com/unicode-org/cldr/raw/release-37/common/annotations/en.xml \
-	-O https://github.com/unicode-org/cldr/raw/release-37/common/annotations/en_001.xml
+	-o en1.xml https://github.com/unicode-org/cldr/raw/release-37/common/annotations/en.xml \
+	-o en2.xml https://github.com/unicode-org/cldr/raw/release-37/common/annotations/en_001.xml \
+	-o en3.xml https://github.com/unicode-org/cldr/raw/release-37/common/annotationsDerived/en.xml \
+	-o en4.xml https://github.com/unicode-org/cldr/raw/release-37/common/annotationsDerived/en_001.xml
 
 jq_code='
 	def to_dict: {(."@cp"): {(if ."@type" == "tts" then "name" else "keywords" end): (."#text")}};
@@ -12,4 +14,4 @@ jq_code='
 	def out: .key + " " + .value.name + " (" + .value.keywords + ")";
 	[ .[].ldml.annotations.annotation[] | to_dict ] | merge | to_entries[] | out
 '
-xq -r -s "$jq_code" en.xml en_001.xml
+xq -r -s "$jq_code" en{1..4}.xml
