67ce0221d4a6afb23c8ed0437c710d2267646b48
Tomas Janousek <tomi@nomi.cz>
2020-11-13 12:46:28 +0000

    Show frecently used emoji first (uses rofi-dmenu-frece)

--- emoji-menu.sh
+++ emoji-menu.sh
@@ -4,7 +4,18 @@ set -eu
 
 cd "$(dirname "$(readlink -f "$0")")"
 
-read -r emoji desc < <(rofi -dmenu -i -p "emoji" -font "mono 20" -width 80 <emoji-data)
+# use https://github.com/liskin/dotfiles/blob/home/bin/rofi-dmenu-frece if available
+if command -v rofi-dmenu-frece &>/dev/null; then
+	function rofi-dmenu {
+		rofi-dmenu-frece --db emoji-frece.db -- "$@"
+	}
+else
+	function rofi-dmenu {
+		rofi -dmenu "$@"
+	}
+fi
+
+read -r emoji desc < <(rofi-dmenu -i -p "emoji" -font "mono 20" -width 80 <emoji-data)
 [[ $emoji && $desc ]]
 
 # wait for focus to return back from rofi
